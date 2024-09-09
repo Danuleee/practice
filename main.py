43dd5e42b7df99d0a5fb070b0c463eeab524a40c
Danuleee <vanea.dan65@mail.ru>
2024-09-09 13:10:11 +0300

    second commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from doctr.io import DocumentFile
 from doctr.models import ocr_predictor
 
 model = ocr_predictor(pretrained=True)
-# PDF
+ 
 doc = DocumentFile.from_pdf("passport.pdf")
 # Analyze
 result = model(doc)
